9097a61c91ed2c4185ae3599449b593535ea800c
Thanatos <Thanatosh@gmail.com>
2024-09-24 12:34:39 +0300

    Part 13.6 Full Commit

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -37,4 +37,19 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+
+  if (blog) {
+    const { likes } = req.body
+    if (typeof likes !== 'undefined') {
+      blog.likes = likes
+    }
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).json({ error: 'Blog not found' })
+  }
+});
+
 module.exports = router
